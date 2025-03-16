256fda7f3c13316117be978b7c19651c87ca5bc4
Bintang Bagus Pangestu <bintangbgs16@gmail.com>
2025-03-17 01:17:01 +0700

    try same root margin for mobile and desktop

--- src/main.js
+++ src/main.js
@@ -98,7 +98,8 @@ const animateOnScroll = (entries) => {
 };
 
 const options = {
-  rootMargin: isMobile ? '100px 0px 60px 0px' : '180px 0px 120px 0px',
+  // rootMargin: isMobile ? '100px 0px 60px 0px' : '180px 0px 120px 0px',
+  rootMargin: '180px 0px 120px 0px',
   threshold: isMobile ? 0.25 : 0.5,
 };
 
